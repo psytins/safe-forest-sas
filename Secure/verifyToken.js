85a963474cca4769fad6cc488ccb0651d6903452
gMartins <45578391+psytins@users.noreply.github.com>
2024-04-25 19:12:07 +0100

    small bug fixed

--- Secure/verifyToken.js
+++ Secure/verifyToken.js
@@ -3,12 +3,12 @@ function verifyToken(req, res, next) {
     const token = req.headers.authorization;
 
     if (!token) {
-        return res.status(401).json({ redirectTo: '/authentication' });
+        return res.redirect('/authentication');
     }
 
     jwt.verify(token, secretKey, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ redirectTo: '/authentication' });
+            return res.redirect('/authentication');
         }
         req.userId = decoded.userId;
         next();
