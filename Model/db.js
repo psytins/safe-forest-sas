4a873611bfabf0f142fa79d071e5fd157764bed0
gMartins <45578391+psytins@users.noreply.github.com>
2024-05-06 12:29:37 +0100

    small changes

--- Model/db.js
+++ Model/db.js
@@ -4,12 +4,12 @@ const mysql = require('mysql2');
 const fs = require('fs');
 
 const db_table = process.env.DB_TABLE;
-const db_user = process.env.DB_LOCAL_USER;
+const db_user = process.env.DB_USER //para mudar para local, usar variavél: DB_LOCAL_USER;
 
-const db_pass = process.env.DB_LOCAL_SECRET;
+const db_pass = process.env.DB_SECRET //para mudar para local, usar variavél: DB_LOCAL_SECRET;
 const cert_path = process.env.GV_DB_PATH;
 
-const host = process.env.DB_LOCAL_HOST;
+const host = process.env.DB_HOST //para mudar para local, usar variavél: DB_LOCAL_HOST;
 
 // Test Connection
 mysql.createConnection(
@@ -19,7 +19,7 @@ mysql.createConnection(
         password: db_pass,
         database: db_table,
         port: 3306,
-        //ssl: { ca: fs.readFileSync(cert_path) }
+        ssl: { ca: fs.readFileSync(cert_path) } // comentar esta linha se for usado a base de dados local.
     });
 
 // Connect to MySQL DB
@@ -27,7 +27,7 @@ const connectionSQL = new Sequelize(db_table, db_user, db_pass, {
     host: host,
     dialect: 'mysql',
     dialectOptions: {
-        //ssl: { ca: fs.readFileSync(cert_path) }
+        ssl: { ca: fs.readFileSync(cert_path) } // comentar esta linha se for usado a base de dados local.
     }
 });
 
